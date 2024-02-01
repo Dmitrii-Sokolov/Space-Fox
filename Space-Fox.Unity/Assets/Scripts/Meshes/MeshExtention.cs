1a3c2be3b549b9ffd8719e622a850b58f8258806
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-02-01 06:49:41 +0100

    MeshExtention

---
+++ Space-Fox.Unity/Assets/Scripts/Meshes/MeshExtention.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace SpaceFox
+{
+    public static class MeshExtention
+    {
+        public static void MakeRibbed(this Mesh mesh)
+        {
+            var oldVertices = mesh.vertices;
+            var triangles = mesh.triangles;
+            var newVertices = new Vector3[triangles.Length];
+
+            for (var i = 0; i < triangles.Length; i++)
+            {
+                newVertices[i] = oldVertices[triangles[i]];
+                triangles[i] = i;
+            }
+
+            mesh.vertices = newVertices;
+            mesh.triangles = triangles;
+
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+        }
+    }
+}
