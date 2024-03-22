f8fa2a8f411fea697ffad3abaa9e42864ddeb2f0
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-03-22 16:52:26 +0100

    Surface noise draft

--- Space-Fox.Unity/Assets/Scripts/Meshes/SphereChunk.cs
+++ Space-Fox.Unity/Assets/Scripts/Meshes/SphereChunk.cs
@@ -276,6 +276,8 @@ namespace SpaceFox
 
         private void OnLateUpdate()
         {
+            //TODO Generate whole sphere when observer is far
+            
             if (RedrawIsNeeded)
             {
                 RedrawIsNeeded = false;
@@ -412,9 +414,6 @@ namespace SpaceFox
         
         private static Mesh GenerateMesh(Region region, MeshPolygoned reference, Vector3 center, float radius)
         {
-            //TODO Generate whole sphere when far
-            //TODO Add height noise
-
             var sector = GetSector(region, reference);
             var mesh = MeshPolygoned.GetPolygon(sector);
             mesh.TransformVertices(GetRelativeHeight);
@@ -435,6 +434,55 @@ namespace SpaceFox
         }
 
         private static Vector3 GetRelativeHeight(Vector3 direction)
-            => direction.normalized;
+        {
+            //TODO Add height noise
+            var normailised = direction.normalized;
+            var debugCellsCount = 4f;
+            var uv = normailised * debugCellsCount;
+
+            var gridId = new Vector3(Mathf.Floor(uv.x), Mathf.Floor(uv.y), Mathf.Floor(uv.z));
+            var gridUV = uv - gridId;
+            //gridUV = quintic(gridUV);
+
+            var p00 = gridId + Vector3.zero;
+            var p01 = gridId + Vector3.right;
+            var p10 = gridId + Vector3.up;
+            var p11 = gridId + Vector3.one;
+
+            var gradP00 = GetRandomGradient(p00);
+            var gradP01 = GetRandomGradient(p01);
+            var gradP10 = GetRandomGradient(p10);
+            var gradP11 = GetRandomGradient(p11);
+
+            var distP00 = gridUV - Vector3.zero;
+            var distP01 = gridUV - Vector3.right;
+            var distP10 = gridUV - Vector3.up;
+            var distP11 = gridUV - Vector3.one;
+
+            var dotP00 = Vector3.Dot(gradP00, distP00);
+            var dotP01 = Vector3.Dot(gradP01, distP01);
+            var dotP10 = Vector3.Dot(gradP10, distP10);
+            var dotP11 = Vector3.Dot(gradP11, distP11);
+
+            var dotP0 = Mathf.Lerp(dotP00, dotP01, gridUV.x);
+            var dotP1 = Mathf.Lerp(dotP10, dotP11, gridUV.x);
+            var dotP = Mathf.Lerp(dotP0, dotP1, gridUV.y);
+
+            return normailised * (dotP * 0.1f + 1f);
+        }
+
+        private static Vector2 GetRandomGradient(Vector2 vector)
+        {
+            vector += 0.01f * Vector2.one;
+            var x = Vector2.Dot(vector, new Vector2(123.4f, 234.5f));
+            var y = Vector2.Dot(vector, new Vector2(234.5f, 345.6f));
+
+            var gradient = new Vector2(x, y);
+            gradient = new Vector2(Mathf.Sin(gradient.x), Mathf.Sin(gradient.y));
+            gradient *= 43758.5453f;
+            gradient = new Vector2(Mathf.Sin(gradient.x), Mathf.Sin(gradient.y));
+
+            return gradient;
+        }
     }
 }
