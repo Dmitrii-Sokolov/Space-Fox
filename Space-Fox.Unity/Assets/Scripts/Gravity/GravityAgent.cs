f4032b9fab45b635fb41f28b64d21059a779a581
Dimitri Sokolov <DimitriSokolov_GitHub@hotmail.com>
2024-03-24 14:26:43 +0100

    Gravity draft

---
+++ Space-Fox.Unity/Assets/Scripts/Gravity/GravityAgent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace SpaceFox
+{
+    public class GravityAgent : DisposableMonoBehaviour
+    {
+        [SerializeField] private Rigidbody Rigidbody;
+
+        [Inject] private readonly GravitySystem GravitySystem;
+
+        public Rigidbody Body => Rigidbody;
+
+        protected override void AwakeBeforeDestroy()
+        {
+            base.AwakeBeforeDestroy();
+
+            GravitySystem.AddAgent(this).While(this);
+        }
+    }
+}
---
+++ Space-Fox.Unity/Assets/Scripts/Gravity/GravitySystem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpaceFox
+{
+    public class GravitySystem : DisposableComposer
+    {
+        private const float GravitationalConstant = 1f; // 6.674×10^−11;
+
+        private readonly List<GravityAgent> Agents = new();
+
+        private GravitySystem(UpdateProxy updateProxy)
+        {
+            updateProxy.FixedUpdate.Subscribe(OnFixedUpdate).While(this);
+
+            AddDisposable(new Subscription(() => Agents.Clear()));
+        }
+
+        public IDisposable AddAgent(GravityAgent agent)
+        {
+            Agents.Add(agent);
+
+            return new Subscription(() => Agents.Remove(agent));
+        }
+
+        private void OnFixedUpdate()
+        {
+            //subject -->> agent
+            foreach (var agent in Agents)
+            {
+                foreach (var subject in Agents)
+                {
+                    if (agent == subject)
+                        continue;
+
+                    var vector = agent.Body.position - subject.Body.position;
+                    var force = GravitationalConstant * agent.Body.mass * subject.Body.mass * vector.normalized / vector.sqrMagnitude;
+                    agent.Body.AddForce(force);
+                }
+            }
+        }
+    }
+}
